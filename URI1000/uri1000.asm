b763d643cb5f33bd39a792d35a4919f05b7dbd37
00lucasm <lucs.r06@gmail.com>
2023-03-10 02:41:06 -0300

    URI1000, initial

---
+++ URI1000/uri1000.asm
@@ -0,0 +1,22 @@
+;; hello world
+
+section .text
+global _start
+
+_start:
+    mov     edx,len
+    mov     ecx,msg
+    mov     ebx,1
+    mov     eax,4
+
+;   (sys_write)
+    int     0x80
+
+    mov     eax,1
+
+; sys_exit
+    int     0x80
+
+section     .data
+    msg     db 'Hello world!',0xa
+    len     equ $ - msg
